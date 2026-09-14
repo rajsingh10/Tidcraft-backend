760c8138e4b9591026ab24a93142497e737eb682
saurav <saurav.fablead@gmail.com>
2026-09-14 16:27:52 +0530

    add renuew and upgrade

--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,8 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/purchases', [ClientPurchaseController::class, 'store']);
         Route::get('/purchases/{uuid}', [ClientPurchaseController::class, 'show']);
         Route::post('/purchases/{uuid}/verify-payment', [ClientPurchaseController::class, 'verifyPayment']);
+        Route::post('/purchases/{uuid}/renew', [ClientPurchaseController::class, 'renew']);
+        Route::post('/purchases/{uuid}/upgrade', [ClientPurchaseController::class, 'upgrade']);
         Route::post('/purchases/{uuid}/domain', [ClientPurchaseController::class, 'setupDomain']);
         Route::get('/payments', [ClientPurchaseController::class, 'payments']);
     });
