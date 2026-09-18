3602e126951da3ae9329c976926d0d495472e344
saurav <saurav.fablead@gmail.com>
2026-09-18 16:57:21 +0530

    migration fix

---
+++ database/migrations/2026_09_18_105600_add_fields_to_email_templates_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->enum('status', ['active', 'inactive'])->default('active')->after('images');
+            
+            $table->unsignedBigInteger('created_by')->nullable()->after('status');
+            $table->unsignedBigInteger('updated_by')->nullable()->after('created_by');
+            $table->unsignedBigInteger('deleted_by')->nullable()->after('updated_by');
+            
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('set null');
+            
+            $table->softDeletes()->after('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+            $table->dropForeign(['deleted_by']);
+            
+            $table->dropColumn([
+                'status',
+                'created_by',
+                'updated_by',
+                'deleted_by',
+                'deleted_at'
+            ]);
+        });
+    }
+};
